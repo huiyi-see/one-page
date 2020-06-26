bb8d4991d37aef988636adfd8c593879b76e8df2
huiyi-see <hy821329@dal.ca>
2020-06-26 16:14:37 -0300

    fixing html warnings based on w3c validator

--- js/index.js
+++ js/index.js
@@ -2,17 +2,8 @@ function func(y){
 	y.classList.toogle("change");
 }
 
-function myFunc(){
-	var y = document.getElementById("nav");
-	if (x.style.display ==="block"){
-		x.style.display ="none";
-	}
-	else
-	{
-		x.style.display = "block";
-	}
-}
 
-$(document).ready(function(){
 
-});
+//$(document).ready(function(){
+
+//});
